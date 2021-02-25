baeb6b71e18932b28aeebda8ee593412a12ebe06
beatrizdossantos <jujukbibi@gmail.com>
2021-02-25 07:43:59 -0300

    AULA 03

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest';
+import { app } from "../app";
+import createConnection from '../database';
+
+describe("Users", () => {
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    it("Should be able to create a new user", async () => {
+        const response = await request(app).post("/users").send({
+            name: "Usuario",
+            email: "user.testando@email.com",
+        });
+
+        expect(response.status).toBe(201);
+    });
+
+    it("Should not be able to create a user with exist email", async () => {
+        const response = await request(app).post("/users").send({
+            name: "Usuario",
+            email: "user.testando@email.com",
+        });
+
+        expect(response.status).toBe(400);
+    });
+
+})
+
